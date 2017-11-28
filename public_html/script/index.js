a851372162f6138fa090bbc6c1ace6889d9f2663
mr.kalinuk <mr.kalinuk@gmail.com>
2017-11-28 16:19:24 +0200

    one

---
+++ public_html/script/index.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$(document).ready(function(){
+//    var widthLen = ($(".head-dinamic").width() / 100);
+//    var heightLen = ($(".head-dinamic").height() / 100);
+//    var res = widthLen * heightLen;
+//    for(var i = 0; i < res;i++){
+//        var miniBlock = document.createElement("div");
+//        miniBlock.className = "mini-block";
+////        miniBlock.style.width = widthLen+"px";
+////        miniBlock.style.height = heightLen+"px";
+//        $(".head-dinamic").append(miniBlock);
+//    }  
+});
